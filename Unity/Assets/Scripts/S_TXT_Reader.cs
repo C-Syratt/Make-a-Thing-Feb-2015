39c4e69301f6708d570330636ba3f85e5dca4d70
JimVincent <1000343@student.sae.edu.au>
2015-02-04 07:50:14 +1000

    Uupdated TXT Reader
    Now has a function to convert names into initials.

--- Unity/Assets/Scripts/S_TXT_Reader.cs
+++ Unity/Assets/Scripts/S_TXT_Reader.cs
@@ -2,15 +2,26 @@
 using System.Collections;
 using System.Collections.Generic;
 
+// employee
+public class Employee
+{
+	public string name = null;
+	public string coffee = null;
+	public int roomNum = null;
+}
+
 public class S_TXT_Reader : MonoBehaviour 
 {
+
 	static public S_TXT_Reader inst;
-	
-	public TextAsset text1;
-	public TextAsset text2;
+
+	public TextAsset txtFirst;
+	public TextAsset txtLast;
+	public TextAsset txtCoffee;
 	
 	private List<string> namesFirst = new List<string>();
 	private List<string> namesLast = new List<string>();
+	private List<string> namesCoffee = new List<string>();
 
 	// Use this for initialization
 	void Awake() 
@@ -19,20 +30,42 @@ public class S_TXT_Reader : MonoBehaviour
 		inst = this;
 		
 		// occupy name lists
-		GenNames(text1, namesFirst);
-		GenNames(text2, namesLast);
+		GenNames(txtFirst, namesFirst);
+		GenNames(txtLast, namesLast);
+		GenNames(txtCoffee, namesCoffee);
 	}
-	
+
+	public Employee NewEmployee()
+	{
+		Employee tempE = new Employee();
+		tempE.name = NewName();
+
+	}
+
 	// return a random name string
-	public string NewName()
+	private string NewName()
 	{
 		Shuffle(namesFirst);
 		Shuffle(namesLast);
 		return namesFirst[0] + " " + namesLast[0];
 	}
+
+	// returns the first char of each word in "B.T." format
+	public string GetInitials(string text)
+	{
+		string initials = null;
+		string[] words = text.Split (' ');
+		foreach(string s in words)
+		{
+			char[] temp = s.ToCharArray();
+			initials += temp[0].ToString() + ".";
+		}
+
+		return initials;
+	}
 	
 	// deserializes the txt to a list of strings
-	public void GenNames(TextAsset txt, List<string> nList)
+	private void GenNames(TextAsset txt, List<string> nList)
 	{
 		string[] lines = txt.text.Split('\n');
 		foreach(string s in lines)
@@ -42,7 +75,7 @@ public class S_TXT_Reader : MonoBehaviour
 	}
 	
 	// Fischer Yates shuffle
-	public void Shuffle(List<string> list)
+	private void Shuffle(List<string> list)
 	{
 		for(int i = list.Count - 1; i > 0; i--)
 		{
