27d9efa045d676771d9e13920963e6eb13eb5020
C-Syratt <c.syratt@aanet.com.au>
2015-02-03 15:38:07 +1000

    Added Coffee Mug to main menu
    Shit rotates.

---
+++ Unity/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	public GameObject featureObject;
+	//sdfuagsdkfuSGf
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		featureObject.transform.Rotate (2.0f, 1f, 4f);
+	}
+}
