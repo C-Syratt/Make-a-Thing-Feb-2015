1176a95c8ed4664a2b321b19c9ba43056fc2c472
Silcoish <silcoish@gmail.com>
2015-02-03 21:39:42 +1000

    UI Stuff
    UI Stuff

--- Unity/Assets/Scripts/CoffeeCart.cs
+++ Unity/Assets/Scripts/CoffeeCart.cs
@@ -5,13 +5,40 @@ using System.Collections;
 public class CoffeeCart : MonoBehaviour {
 
 	[SerializeField] float maxDistance = 5f;
-	int[] roomID;
+	public int[] roomID;
 	[SerializeField] GameObject player;
 	[SerializeField] Canvas notepadCanvas;
 
 	void Start()
 	{
 		roomID = new int[5];
+		for(int i = 0; i < roomID.Length; i++)
+		{
+			bool contains = true;
+			while(contains)
+			{
+				int nextInt = Random.Range(0, 5);
+				if(CheckContains(nextInt))
+				{
+					contains = true;
+				}
+				else
+				{
+					roomID[i] = nextInt;
+				}
+			}
+			contains = true;
+		}
+	}
+
+	bool CheckContains(int a)
+	{
+		for(int i = 0; i < roomID.Length; i++)
+		{
+			if(roomID[i] == a)
+				return true;
+		}
+		return false;
 	}
 
 	void Update () {
