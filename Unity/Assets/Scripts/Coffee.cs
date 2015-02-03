6cd9fb22e09cecf958d7d5c98f9e57212c1ded79
Silcoish <silcoish@gmail.com>
2015-02-03 15:40:25 +1000

    Character Coffee, Coffee Cart Started
    Character can hold coffee in both hands, Coffee Cart UI started, Warm
    cold hot enum done

---
+++ Unity/Assets/Scripts/Coffee.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coffee : MonoBehaviour {
+	
+	[SerializeField] float deliveryTimeSeconds = 300f;
+	float counter = 0f;
+	[SerializeField] int percentageWarm = 100;
+	[SerializeField] HeatEnum temp = HeatEnum.HOT;
+
+	void Update () {
+		counter += Time.deltaTime;
+		percentageWarm = (int) (100 - (counter / deliveryTimeSeconds * 100f));
+		if(percentageWarm <= 20)
+		{
+			temp = HeatEnum.COLD;
+		}
+		else if(percentageWarm <= 60)
+		{
+			temp = HeatEnum.WARM;
+		}
+		else
+		{
+			temp = HeatEnum.HOT;
+		}
+	}
+}
---
+++ Unity/Assets/Scripts/CoffeeCart.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CoffeeCart : MonoBehaviour {
+
+	[SerializeField] float maxDistance = 5f;
+	int[] roomID;
+	[SerializeField] GameObject player;
+	[SerializeField] Canvas notepadCanvas;
+
+	void Start()
+	{
+		roomID = new int[5];
+	}
+
+	void Update () {
+		if(Vector3.Distance(transform.position, player.transform.position) <= maxDistance)
+		{
+			notepadCanvas.enabled = true;
+		}
+		else
+		{
+			notepadCanvas.enabled = false;
+		}
+	}
+
+	void OptionOne()
+	{
+
+	}
+
+	void OptionTwo()
+	{
+
+	}
+
+	void OptionThree()
+	{
+
+	}
+
+	void OptionFour()
+	{
+
+	}
+
+	void OptionFive()
+	{
+
+	}
+}
