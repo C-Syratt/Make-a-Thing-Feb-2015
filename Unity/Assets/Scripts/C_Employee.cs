3659c67dc8c7c5f8b97167ecf144c491f4961719
JimVincent <1000343@student.sae.edu.au>
2015-02-04 12:31:44 +1000

    Corey and Jim's Paired Programming

---
+++ Unity/Assets/Scripts/C_Employee.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class C_Employee : MonoBehaviour 
+{
+	public string firstName;
+	public string lastName;
+	public int roomNum;
+	public string coffee;
+	public bool isPerson = false;
+	public GameObject person = null;
+
+	void Start()
+	{
+		if(!isPerson)
+		{
+			S_Employee_Manager.inst.employeeList.Add(this);
+		}
+	}
+
+	public void SetPersonData()
+	{
+		if(person != null)
+		{
+			C_Employee tempE = person.GetComponent<C_Employee>();
+			tempE.name = name;
+			tempE.roomNum = roomNum;
+			tempE.coffee = coffee;
+			tempE.isPerson = true;
+		}
+	}
+
+	public void SetData(string first, string last, string coff)
+	{
+		firstName = first;
+		lastName = last;
+		coffee = coff;
+	}
+}
---
+++ Unity/Assets/Scripts/S_Employee_Manager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class S_Employee_Manager : MonoBehaviour 
+{
+	static public S_Employee_Manager inst;
+
+	void Awake()
+	{
+		inst = this;
+	}
+
+	public List<C_Employee> employeeList = new List<C_Employee>();
+
+	// Use this for initialization
+	void Start () 
+	{
+		AssignDoors();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void AssignDoors()
+	{
+		for(int i = 0; i < employeeList.Count; i++)
+		{
+			print ("Looped");
+			string tempF = S_TXT_Reader.inst.NewFirstName();
+			string tempL = S_TXT_Reader.inst.NewLastName();
+			string tempC = S_TXT_Reader.inst.GetCoffee(tempF);
+			employeeList[i].SetData(tempF, tempL, tempC);
+		}
+	}
+}
