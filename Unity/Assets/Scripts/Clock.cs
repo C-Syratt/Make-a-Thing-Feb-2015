0f700ff6d514aeb68a6403eb90c418f460efffe1
Richard Stein <105014@student.sae.edu.au>
2015-02-06 15:31:33 +1000

    Fixed clock and script
    Set clock to 9.00 and fixed the script to suit the little hand changing
    one hour for every sweep of the big hand

--- Unity/Assets/Scripts/Clock.cs
+++ Unity/Assets/Scripts/Clock.cs
@@ -8,6 +8,6 @@ public class Clock : MonoBehaviour {
 
 	void Update () {
 		minuteHand.transform.Rotate(new Vector3(0f, 0f, (360f / 60f) * -Time.deltaTime));
-		hourHand.transform.Rotate(new Vector3(0f, 0f, (360f / 60f / 12f / 2f) * -Time.deltaTime));
+		hourHand.transform.Rotate(new Vector3(0f, 0f, (360f / 60f / 12f ) * -Time.deltaTime));
 	}
 }
